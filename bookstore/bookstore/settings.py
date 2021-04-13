e9e5ed59d3754e58cdaccb4468e44a89001d98d7
rasheedkotoor <rasheedkmkotoor@gmail.com>
2021-04-13 08:45:30 +0530

    debgu t

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'k7a)pxs1t0^jme@u5w#3qwu6u-d=ua#$6wy4e95+tvvp@*r1&k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
